6b69cf5554598eaffd921b9b3ed98ce9ad9272cf
sjteckers <shivanshu@intellewings.com>
2022-09-03 11:43:52 +0530

    changes for heroku deployment

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/sj-portfolio'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/sj-portfolio/'}),
-);
-
-// Start the app by listening on the default Heroku port
+app.use(express.static(__dirname + '/dist/sj-portfolio'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/sj-portfolio/index.html'));});
 app.listen(process.env.PORT || 8080);
+console.log(process.env.PORT);
